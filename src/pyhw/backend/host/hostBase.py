12f5a34c8c50436b054307a50542a5377993ea95
xiaoran <xiaoran2495897824@163.com>
2024-08-13 19:54:05 +0800

    update hostBase.py

--- src/pyhw/backend/host/hostBase.py
+++ src/pyhw/backend/host/hostBase.py
@@ -1,2 +1,18 @@
+from .linux import HostDetectLinux
+from .macos import HostDetectMacOS
+from .windows import HostDetectWindows
+from ...pyhwException import OSUnsupportedException
+
 class HostDetect:
-    pass
+    def __init__(self, os):
+        self.OS = os
+
+    def getHostInfo(self):
+        if self.OS == "linux":
+            pass
+        elif self.OS == "macos":
+            pass
+        elif self.OS == "windows":
+            pass
+        else:
+            raise OSUnsupportedException("Unsupported operating system")

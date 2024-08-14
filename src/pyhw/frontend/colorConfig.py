2e0a2e06717afbe4e8956cb6128481ee7a128215
xiaoran <xiaoran2495897824@163.com>
2024-08-14 15:53:20 +0800

    add: color config for macos

---
+++ src/pyhw/frontend/colorConfig.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from .colorSet import ColorSet
+
+
+@dataclass
+class ColorConfig:
+    pass
+
+
+@dataclass
+class ColorConfigSetA:
+    pass
+
+
+@dataclass
+class ColorConfigSetD:
+    pass
+
+
+@dataclass
+class ColorConfigSetM:
+    macOS = {
+        "colors": [
+            ColorSet.COLOR_FG_GREEN,
+            ColorSet.COLOR_FG_YELLOW,
+            ColorSet.COLOR_FG_RED,
+            ColorSet.COLOR_FG_MAGENTA,
+            ColorSet.COLOR_FG_BLUE
+        ],
+        "colorKeys": ColorSet.COLOR_FG_YELLOW,
+        "colorTitle":  ColorSet.COLOR_FG_GREEN
+    }
+

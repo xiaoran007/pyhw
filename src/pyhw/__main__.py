5d4317f7bb79eee2eab436e9c63ffd9f3fe99426
xiaoran <xiaoran2495897824@163.com>
2024-08-15 21:55:39 +0800

    update: package command line entry point and module entry point

--- src/pyhw/__main__.py
+++ src/pyhw/__main__.py
@@ -1,5 +1,25 @@
+from .frontend import Printer
+from .backend import Data
+from .backend.title import TitleDetect
+from .backend.host import HostDetect
+from .backend.kernel import KernelDetect
+from .backend.shell import ShellDetect
+from .backend.uptime import UptimeDetect
+from .backend.os import OSDetect
+from .pyhwUtil import createDataString
+
+
 def main():
-    print("pass")
+    print("This is a test version of PyHw. Currently, it only supports Linux debian based OS")
+    data = Data()
+    data.title = TitleDetect(os="linux").getTitle().title
+    data.Host = HostDetect(os="linux").getHostInfo().model
+    data.Kernel = KernelDetect(os="linux").getKernelInfo().kernel
+    data.Shell = ShellDetect(os="linux").getShellInfo().info
+    data.Uptime = UptimeDetect(os="linux").getUptime().uptime
+    data.OS = OSDetect(os="linux").getOSInfo().prettyName
+
+    Printer(logo_os=OSDetect(os="linux").getOSInfo().id, data=createDataString(data)).cPrint()
 
 
 if __name__ == "__main__":

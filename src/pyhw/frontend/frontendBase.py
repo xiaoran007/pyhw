4a3fc3c52e3e953d572b61ce6c98b978a8799662
xiaoran <xiaoran2495897824@163.com>
2024-08-16 22:55:13 +0800

    update: support more complex logo color settings

--- src/pyhw/frontend/frontendBase.py
+++ src/pyhw/frontend/frontendBase.py
@@ -10,25 +10,30 @@ class Printer:
         self.__config = ColorConfigSet(logo_os).getColorConfigSet()
         self.__logo_lines = self.__logo.split("\n")
         self.__data_lines = self.__data.strip().split("\n")
+        self.__line_length = []
         self.__processed_logo_lines = []
         self.__processed_data_lines = []
         self.__combined_lines = []
         self.__logo_color_indexes = {}
-        self.__reg = r'\$\d+'
+        self.__reg = r'\$(\d)'
 
     def cPrint(self):
         self.__LogoPreprocess()
         self.__DataPreprocess()
-        max_len_logo = max(len(i) for i in self.__processed_logo_lines)
+        # max_len_logo = max(len(i) for i in self.__processed_logo_lines)
+        max_len_logo = max(self.__line_length)
         for i, (logo_line, data_line) in enumerate(zip(self.__processed_logo_lines, self.__processed_data_lines)):
-            if i in self.__logo_color_indexes.keys():
-                combined_line = colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(self.__logo_color_indexes[i]) + logo_line.ljust(max_len_logo) + colorSuffix() + data_line
-            else:
-                combined_line = logo_line.ljust(max_len_logo) + data_line
+            # if i in self.__logo_color_indexes.keys():
+            #     combined_line = colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(self.__logo_color_indexes[i]) + logo_line.ljust(max_len_logo) + colorSuffix() + data_line
+            # else:
+            #     combined_line = logo_line.ljust(max_len_logo) + data_line
+            # combined_line = logo_line.ljust(max_len_logo) + data_line
+            combined_line = logo_line + " " * (max_len_logo - self.__line_length[i] + 1) + data_line
             self.__combined_lines.append(combined_line)
 
         for i, logo_line in enumerate(self.__processed_logo_lines[len(self.__processed_data_lines):], start=len(self.__processed_data_lines)):
-            self.__combined_lines.append(colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(self.__logo_color_indexes[i]) + logo_line + colorSuffix())
+            # self.__combined_lines.append(colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(self.__logo_color_indexes[i]) + logo_line + colorSuffix())
+            self.__combined_lines.append(logo_line)
 
         for data_line in self.__processed_data_lines[len(self.__processed_logo_lines):]:
             self.__combined_lines.append(" " * max_len_logo + data_line)
@@ -36,19 +41,25 @@ class Printer:
         print("\n".join(self.__combined_lines))
 
     def __LogoPreprocess(self):
-        # TODO: re-write this part to support more complex logo color settings
-        i = 0
-        color = self.__config.get("colors")[0]
+        global_color = self.__config.get("colors")[0]
         for logo_line in self.__logo_lines:
-            match = re.search(self.__reg, logo_line)
-            if match:
-                color = self.__config.get("colors")[int(match[0][-1]) - 1]
-                self.__processed_logo_lines.append(re.sub(self.__reg, "", logo_line))
-                self.__logo_color_indexes[i] = color
+            matches = re.findall(pattern=self.__reg, string=logo_line)
+            color_numbers = len(matches)
+            line_length = len(logo_line)
+            if color_numbers > 0:
+                colors = [int(match) - 1 for match in matches]  # color indexes
+                temp_line = colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(global_color) + logo_line + colorSuffix()
+                for color in colors:
+                    temp_line = temp_line.replace(f"${color+1}", colorPrefix(self.__config.get("colors")[color]))
+                global_color = self.__config.get("colors")[colors[-1]]  # set the global color to the last used color
             else:
-                self.__processed_logo_lines.append(logo_line)
-                self.__logo_color_indexes[i] = color
-            i += 1
+                temp_line = colorPrefix(ColorSet.COLOR_MODE_BOLD) + colorPrefix(global_color) + logo_line + colorSuffix()
+            flags_number = len(re.findall(pattern=r'\$\$', string=logo_line)) + len(re.findall(pattern=r'\$\0', string=logo_line))
+            if flags_number > 0:
+                temp_line = temp_line.replace("$$", "$")
+                temp_line = temp_line.replace("$\0", "$")
+            self.__line_length.append(line_length - 2 * color_numbers - flags_number)
+            self.__processed_logo_lines.append(temp_line)
 
     def __DataPreprocess(self):
         header_color = self.__config.get("colorTitle")

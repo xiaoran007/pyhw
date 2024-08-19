2c4eb65bb9af1b7a7ef92dd332723b81e59a6415
xiaoran <xiaoran2495897824@163.com>
2024-08-19 17:04:04 +0800

    update: sysctl helper int

--- src/pyhw/pyhwUtil/__init__.py
+++ src/pyhw/pyhwUtil/__init__.py
@@ -1,4 +1,4 @@
 from .pyhwUtil import getOS, getArch, createDataString, selectOSLogo
-from .sysctlUtil import sysctlGet
+from .sysctlUtil import sysctlGetString, sysctlGetInt
 
-__all__ = ["getOS", "getArch", "createDataString", "selectOSLogo", "sysctlGet"]
+__all__ = ["getOS", "getArch", "createDataString", "selectOSLogo", "sysctlGetString", "sysctlGetInt"]
--- src/pyhw/pyhwUtil/sysctlUtil.py
+++ src/pyhw/pyhwUtil/sysctlUtil.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def sysctlGet(key):
+def __sysctlGet(key):
     """
     Get value from sysctl by key.
     :param key: str, sysctl key
@@ -12,3 +12,26 @@ def sysctlGet(key):
     except subprocess.SubprocessError:
         output = ""
     return output
+
+
+def sysctlGetString(key):
+    """
+    Get value from sysctl by key.
+    :param key: str, sysctl key
+    :return: value of sysctl key or "" if not found
+    """
+    return __sysctlGet(key)
+
+
+def sysctlGetInt(key):
+    """
+    Get value from sysctl by key.
+    :param key: str, sysctl key
+    :return: value of sysctl key as int or None if not found
+    """
+    value = __sysctlGet(key)
+    try:
+        res = int(value)
+    except Exception:
+        res = None
+    return res

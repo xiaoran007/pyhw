46c487934c4a4ccd415ff970151e9e185c022192
xiaoran <xiaoran2495897824@163.com>
2024-08-20 14:30:35 +0800

    fix: host detection on arm desktop (vm)

--- src/pyhw/backend/host/linux.py
+++ src/pyhw/backend/host/linux.py
@@ -3,6 +3,7 @@
 """
 from ...pyhwUtil import getArch
 from .hostInfo import HostInfo
+import os
 
 
 class HostDetectLinux:
@@ -25,11 +26,23 @@ class HostDetectLinux:
             except FileNotFoundError:
                 pass
         elif self.__arch in ["aarch64", "arm32"]:
-            try:
-                with open("/sys/firmware/devicetree/base/model", "r") as f:
-                    self.__hostInfo.model = f.read().strip()
-            except FileNotFoundError:
-                pass
+            # try to find dmi folder since some arm based desktops and servers may have same structure as x86_64 machines.
+            if os.path.exists("/sys/devices/virtual/dmi/id"):
+                try:
+                    with open("/sys/devices/virtual/dmi/id/product_name", "r") as f:
+                        self.__hostInfo.name = f.read().strip()
+                    with open("/sys/devices/virtual/dmi/id/product_version", "r") as f:
+                        self.__hostInfo.version = f.read().strip()
+                    self.__hostInfo.model = self.__hostInfo.name + " " + self.__hostInfo.version
+                except FileNotFoundError:
+                    pass
+            else:
+                # some single board computers may not have dmi folder, try to find model name in device tree.
+                try:
+                    with open("/sys/firmware/devicetree/base/model", "r") as f:
+                        self.__hostInfo.model = f.read().strip()
+                except FileNotFoundError:
+                    pass
 
     def __getHostFamily(self):
         pass

9dd927fc664fb089fee5858d69c961d280c6700d
xiaoran <xiaoran2495897824@163.com>
2024-10-20 14:35:45 +0800

    update: NIC info on macOS

--- src/pyhw/backend/nic/macos.py
+++ src/pyhw/backend/nic/macos.py
@@ -7,12 +7,21 @@ class NICDetectMacOS:
         self.__nicInfo = NICInfo()
 
     def getNICInfo(self):
-        self.__nicInfo.nics.append("en0")
-        self.__nicInfo.number = 1
+        self.__getNICInfo()
         return self.__nicInfo
 
     def __getNICInfo(self):
         # Placeholder for a more advanced method.
-        interfaces = subprocess.run(["bash", "-c", "route get default | grep interface"], capture_output=True, text=True).stdout.strip()
+        try:
+            interface = subprocess.run(["bash", "-c", "route get default | grep interface"], capture_output=True, text=True).stdout.strip().split(":")[1]
+            if_ip = subprocess.run(["bash", "-c", f"ipconfig getifaddr {interface}"], capture_output=True, text=True).stdout.strip()
+            self.__nicInfo.nics.append(f"{interface} @ {if_ip}")
+            self.__nicInfo.number += 1
+        except:
+            self.__handleError()
+
+    def __handleError(self):
+        self.__nicInfo.nics.append("en0")
+        self.__nicInfo.number = 1
 
 

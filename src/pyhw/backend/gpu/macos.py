ef9be969ce9b8c7c497d7feda57b46dd7cf75776
xiaoran <xiaoran2495897824@163.com>
2024-10-25 16:07:19 +0800

    update: enable new swift backend

--- src/pyhw/backend/gpu/macos.py
+++ src/pyhw/backend/gpu/macos.py
@@ -2,6 +2,8 @@ from .gpuInfo import GPUInfo
 from ...pyhwUtil import getArch
 import json
 import subprocess
+import ctypes
+from pathlib import Path
 
 
 class GPUDetectMacOS:
@@ -32,24 +34,49 @@ class GPUDetectMacOS:
             self.__gpuInfo.gpus.append(f'{gpu.get("sppci_model")} ({gpu.get("sppci_cores")} cores) [SOC Integrated]')
 
     def __getGPUIntel(self):
-        gpus = list()
-        try:
-            gpu_info_dict = json.loads(subprocess.check_output(["system_profiler", "SPDisplaysDataType", "-json"]))
-            if 'SPDisplaysDataType' in gpu_info_dict:
-                gpus = gpu_info_dict['SPDisplaysDataType']
-                self.__gpuInfo.number = len(gpus)
-            else:
-                pass
-        except Exception:
-            return
+        if self.__getGPUIOKit():
+            pass
+        else:
+            gpus = list()
+            try:
+                gpu_info_dict = json.loads(subprocess.check_output(["system_profiler", "SPDisplaysDataType", "-json"]))
+                if 'SPDisplaysDataType' in gpu_info_dict:
+                    gpus = gpu_info_dict['SPDisplaysDataType']
+                    self.__gpuInfo.number = len(gpus)
+                else:
+                    pass
+            except Exception:
+                return
 
-        for gpu in gpus:
-            if self.__handleVendor(gpu.get("spdisplays_vendor")) == "Intel":    # Integrated GPU
-                self.__gpuInfo.gpus.append(f'{gpu.get("sppci_model")} [CPU Integrated]')
-            elif self.__handleVendor(gpu.get("spdisplays_vendor")) == "AMD":    # dGPU
-                self.__gpuInfo.gpus.append(f'{gpu.get("sppci_model")} {gpu.get("spdisplays_vram")} [Discrete]')
-            elif self.__handleVendor(gpu.get("spdisplays_vendor")) == "Nvidia":    # Since current macOS does not support NVIDIA GPUs, this condition is not applicable
-                pass
+            for gpu in gpus:
+                if self.__handleVendor(gpu.get("spdisplays_vendor")) == "Intel":    # Integrated GPU
+                    self.__gpuInfo.gpus.append(f'{gpu.get("sppci_model")} [CPU Integrated]')
+                elif self.__handleVendor(gpu.get("spdisplays_vendor")) == "AMD":    # dGPU
+                    self.__gpuInfo.gpus.append(f'{gpu.get("sppci_model")} {gpu.get("spdisplays_vram")} [Discrete]')
+                elif self.__handleVendor(gpu.get("spdisplays_vendor")) == "Nvidia":    # Since current macOS does not support NVIDIA GPUs, this condition is not applicable
+                    pass
+
+    def __getGPUIOKit(self):
+        try:
+            package_root = Path(__file__).resolve().parent.parent.parent
+            lib = ctypes.CDLL(f"{package_root}/library/lib/iokitGPULib.dylib")
+            lib.getGPUInfo.restype = ctypes.c_char_p
+            gpu_info = lib.getGPUInfo()
+            gpus = gpu_info.decode('utf-8').split("; ")
+            self.__gpuInfo.number = len(gpus)
+            for gpu in gpus:
+                info_list = gpu.split(", ")
+                model = info_list[0]
+                vendor_id = info_list[1]
+                vram = int(info_list[2]) / 1024
+                if self.__handleVendorID(vendor_id) == "Intel":    # Integrated GPU
+                    self.__gpuInfo.gpus.append(f'{model} [CPU Integrated]')
+                elif self.__handleVendorID(vendor_id) == "AMD":    # dGPU
+                    self.__gpuInfo.gpus.append(f'{model} {vram} GB [Discrete]')
+            return True
+        except Exception as e:
+            # print(f"An error occurred while getting GPU info using IOKit: {e}")
+            return False
 
     @staticmethod
     def __handleVendor(vendor):
@@ -61,3 +88,13 @@ class GPUDetectMacOS:
             return "AMD"
         else:
             return vendor
+
+    @staticmethod
+    def __handleVendorID(vendor_id):
+        if vendor_id == "0x8086":
+            return "Intel"
+        elif vendor_id == "0x1002":
+            return "AMD"
+        else:
+            return vendor_id
+

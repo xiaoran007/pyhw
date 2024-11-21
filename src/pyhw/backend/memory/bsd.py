024d9c4356d0642bd5a90e62db6864a06c2a2651
xiaoran <xiaoran2495897824@163.com>
2024-11-21 18:28:00 +0800

    Fix: private mem

--- src/pyhw/backend/memory/bsd.py
+++ src/pyhw/backend/memory/bsd.py
@@ -3,9 +3,9 @@ from .linux import MemoryDetectLinux
 
 class MemoryDetectBSD(MemoryDetectLinux):
     def __init__(self):
-        super().__init__()
+        MemoryDetectLinux.__init__(self)
 
     def getMemoryInfo(self):
-        self.__memoryInfo.memory = f"Pass"
-        return self.__memoryInfo
+        self._memoryInfo.memory = f"Pass"
+        return self._memoryInfo
 
--- src/pyhw/backend/memory/linux.py
+++ src/pyhw/backend/memory/linux.py
@@ -3,22 +3,22 @@ from .memoryInfo import MemoryInfo
 
 class MemoryDetectLinux:
     def __init__(self):
-        self.__memoryInfo = MemoryInfo()
+        self._memoryInfo = MemoryInfo()
 
     def getMemoryInfo(self):
-        self.__getMemory()
-        self.__memoryInfo.memory = f"{self.__memoryInfo.used} MiB / {self.__memoryInfo.total} MiB"
-        return self.__memoryInfo
+        self._getMemory()
+        self._memoryInfo.memory = f"{self._memoryInfo.used} MiB / {self._memoryInfo.total} MiB"
+        return self._memoryInfo
 
-    def __getMemory(self):
+    def _getMemory(self):
         try:
             with open("/proc/meminfo", "r") as file:
                 for line in file:
                     if line.startswith("MemTotal:"):
-                        self.__memoryInfo.total = round(float(line.split(":")[1].strip()[:-3]) / 1024, 2)
+                        self._memoryInfo.total = round(float(line.split(":")[1].strip()[:-3]) / 1024, 2)
                     elif line.startswith("MemAvailable:"):
-                        self.__memoryInfo.available = round(float(line.split(":")[1].strip()[:-3]) / 1024, 2)
-                self.__memoryInfo.used = round(self.__memoryInfo.total - self.__memoryInfo.available, 2)
+                        self._memoryInfo.available = round(float(line.split(":")[1].strip()[:-3]) / 1024, 2)
+                self._memoryInfo.used = round(self._memoryInfo.total - self._memoryInfo.available, 2)
         except FileNotFoundError:
             pass
 

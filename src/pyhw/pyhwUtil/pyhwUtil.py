fc89e44da7c8abc0f11d89e970945a750ed5a971
xiaoran <xiaoran2495897824@163.com>
2024-11-21 17:45:10 +0800

    Update: enable freebsd

--- src/pyhw/pyhwUtil/pyhwUtil.py
+++ src/pyhw/pyhwUtil/pyhwUtil.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 def getOS():
     """
     Get the os type in lower case.
-    :return: str, os type, value in [windows, linux, macos, unknown].
+    :return: str, os type, value in [windows, linux, macos, freebsd, unknown].
     """
     system = platform.system()
     if system == "Windows":
@@ -16,6 +16,8 @@ def getOS():
         return "linux"
     elif system == "Darwin":
         return "macos"
+    elif system == "FreeBSD":
+        return "freebsd"
     else:
         return "unknown"
 

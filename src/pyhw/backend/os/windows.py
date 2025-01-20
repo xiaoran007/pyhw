e827ec09e2f13853399fffca9ebb2782cd911c52
xiaoran <xiaoran2495897824@163.com>
2025-01-21 01:43:09 +0800

    Update: Windows os

--- src/pyhw/backend/os/windows.py
+++ src/pyhw/backend/os/windows.py
@@ -1,5 +1,7 @@
 from .osInfo import OSInfo
+from ...pyhwException import BackendException
 import platform
+import winreg
 
 
 class OSDetectWindows:
@@ -11,7 +13,8 @@ class OSDetectWindows:
         release = platform.release()
         edition = platform.win32_edition()
         machine = platform.machine()
-        self._osInfo.prettyName = f"{system} {release} ({edition}) {machine}"
+        display = self.__get_windows_version()
+        self._osInfo.prettyName = f"{system} {release} {display} ({edition}) {machine}"
         if release == "10":
             self._osInfo.id = "windows_10"
         elif release == "11":
@@ -19,4 +22,14 @@ class OSDetectWindows:
         else:
             self._osInfo.id = "windows_old"
         return self._osInfo
+
+    @staticmethod
+    def __get_windows_version():
+        try:
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion")
+            display_version, _ = winreg.QueryValueEx(key, "DisplayVersion")
+            winreg.CloseKey(key)
+            return str(display_version)
+        except:
+            raise BackendException("Unable to determine Windows kernel version.")
 
--- src/windows_test.py
+++ src/windows_test.py
@@ -21,13 +21,13 @@ def main():
     #     print(f"Only Linux, macOS, and FreeBSD is supported for now. Current os: {current_os}")
     #     return
     data = Data()
-    # data.title = TitleDetect(os=current_os).getTitle().title
+    data.title = TitleDetect(os=current_os).getTitle().title
     # data.Host = HostDetect(os=current_os).getHostInfo().model
-    # data.Kernel = KernelDetect(os=current_os).getKernelInfo().kernel
-    # data.Shell = ShellDetect(os=current_os).getShellInfo().info
+    data.Kernel = KernelDetect(os=current_os).getKernelInfo().kernel
+    data.Shell = ShellDetect(os=current_os).getShellInfo().info
     # data.Uptime = UptimeDetect(os=current_os).getUptime().uptime
-    # data.OS = OSDetect(os=current_os).getOSInfo().prettyName
-    data.CPU = CPUDetect(os=current_os).getCPUInfo().cpu
+    data.OS = OSDetect(os=current_os).getOSInfo().prettyName
+    # data.CPU = CPUDetect(os=current_os).getCPUInfo().cpu
     # gpu_info = GPUDetect(os=current_os).getGPUInfo()
     # if gpu_info.number > 0:
     #     data.GPU = gpu_info.gpus
@@ -39,8 +39,8 @@ def main():
     # if npu_info.number > 0:
     #     data.NPU = npu_info.npus
     #
-    # Printer(logo_os=selectOSLogo(OSDetect(os=current_os).getOSInfo().id), data=createDataString(data)).cPrint()
-    print(data.CPU)
+    Printer(logo_os=selectOSLogo(OSDetect(os=current_os).getOSInfo().id), data=createDataString(data)).cPrint()
+    # print(data.CPU)
 
 
 if __name__ == "__main__":

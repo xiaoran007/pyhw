d93eac3cfbe3a2539c618a0975e0b2be117e9b85
xiaoran <xiaoran2495897824@163.com>
2025-02-19 16:29:06 +0800

    Update: BSD mem detect

--- src/pyhw/backend/memory/bsd.py
+++ src/pyhw/backend/memory/bsd.py
@@ -1,11 +1,25 @@
 from .linux import MemoryDetectLinux
+from ...pyhwUtil import sysctlGetInt
 
 
 class MemoryDetectBSD(MemoryDetectLinux):
     def __init__(self):
         MemoryDetectLinux.__init__(self)
+        self.__page_size = 0
+        self.__physical_memory = 0
+        self.__free_pages = 0
+        self.__free_memory = 0
 
     def getMemoryInfo(self):
-        self._memoryInfo.memory = f"Pass"
+        self.__getMemorySysctl()
+        self._memoryInfo.total = self.__physical_memory / 1024 / 1024  # Convert bytes to MiB
+        self._memoryInfo.used = (self.__physical_memory - self.__free_memory) / 1024 / 1024  # Convert bytes to MiB
+        self._memoryInfo.memory = f"{round(self._memoryInfo.used, 2)} MiB / {round(self._memoryInfo.total, 2)} MiB"
         return self._memoryInfo
+
+    def __getMemorySysctl(self):
+        self.__page_size = sysctlGetInt("hw.pagesize")
+        self.__physical_memory = sysctlGetInt("hw.physmem")
+        self.__free_pages = sysctlGetInt("vm.stats.vm.v_free_count")
+        self.__free_memory = self.__free_pages * self.__page_size
 

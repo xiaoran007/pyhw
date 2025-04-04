2d26154e0f4e7e3c03297ac1bf6e73094969cbb4
xiaoran <xiaoran2495897824@163.com>
2025-04-04 23:02:55 +0800

    Update: nvml.h (not official one)

---
+++ src/pyhw/library/nvmlGPULib/nvml.h
@@ -0,0 +1,31 @@
+#ifndef NVML_H
+#define NVML_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NVML_DEVICE_NAME_BUFFER_SIZE                  64
+
+typedef enum nvmlReturn_enum {
+    NVML_SUCCESS = 0,
+    NVML_ERROR_UNKNOWN = 999
+} nvmlReturn_t;
+
+typedef struct nvmlDevice_st* nvmlDevice_t;
+
+const char* nvmlErrorString(nvmlReturn_t result);
+
+nvmlReturn_t nvmlInit(void);
+nvmlReturn_t nvmlShutdown(void);
+nvmlReturn_t nvmlDeviceGetCount(unsigned int* deviceCount);
+nvmlReturn_t nvmlDeviceGetHandleByIndex(unsigned int index, nvmlDevice_t* device);
+nvmlReturn_t nvmlDeviceGetName(nvmlDevice_t device, char* name, unsigned int length);
+nvmlReturn_t nvmlDeviceGetUtilizationRates(nvmlDevice_t device, void* utilization);
+nvmlReturn_t nvmlDeviceGetNumGpuCores(nvmlDevice_t device, unsigned int* numCores);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

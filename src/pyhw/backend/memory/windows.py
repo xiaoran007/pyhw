00fdd1b6a71c11dbbf3bdaffe4988000f152fe41
xiaoran Mac Studio M1 Max <xiaoran2495897824@163.com>
2025-10-31 16:30:56 +0800

    Update (W): convert memory size calculations from MiB to GiB

--- src/pyhw/backend/memory/windows.py
+++ src/pyhw/backend/memory/windows.py
@@ -10,7 +10,7 @@ class MemoryDetectWindows:
 
     def getMemoryInfo(self):
         self._getMemory()
-        self._memoryInfo.memory = f"{self._memoryInfo.used} MiB / {self._memoryInfo.total} MiB"
+        self._memoryInfo.memory = f"{self._memoryInfo.used} GiB / {self._memoryInfo.total} GiB"
         return self._memoryInfo
 
     def _getMemory(self):
@@ -21,8 +21,8 @@ class MemoryDetectWindows:
             raise BackendException("Failed to get memory information on Windows.")
 
         res = json.loads(result.stdout)
-        free_memory = int(res['FreePhysicalMemory']) / 1024
-        total_memory = int(res['TotalVisibleMemorySize']) / 1024
+        free_memory = int(res['FreePhysicalMemory']) / 1024 / 1024
+        total_memory = int(res['TotalVisibleMemorySize']) / 1024 / 1024
         used_memory = total_memory - free_memory
         self._memoryInfo.total = round(total_memory, 2)
         self._memoryInfo.available = round(free_memory, 2)
